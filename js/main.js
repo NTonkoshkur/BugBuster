71568afa22c90e144d99cbc8b6c127f8823f2c10
nesteaniko <straydangerous@gmail.com>
2024-10-27 11:07:34 +0200

    UI

--- js/main.js
+++ js/main.js
@@ -142,11 +142,3 @@ faqs.forEach(faq => {
         faq.classList.toggle('active');
     });
 });
-
-/* Binotel */
-
-(function(d, w, s) {
-    var widgetHash = 'yEaYLo340SJSAbUx3kGr', bch = d.createElement(s); bch.type = 'text/javascript'; bch.async = true;
-    bch.src = '//widgets.binotel.com/chat/widgets/' + widgetHash + '.js';
-    var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(bch, sn);
-})(document, window, 'script');
